f6cb32069a79556148cee277756a589e6d46d933
Yun-Li Hsieh <hsieh5709@calculon.mpimp-golm.mpg.de>
2024-09-05 17:32:28 +0200

    MICOM

---
+++ Program/matlab/MICOM/MICOM_max_growth.m
@@ -0,0 +1,64 @@
+function solution = MICOM_max_growth(model, abTable)
+% Function for finding the maximum community growth rate.
+% Input:
+%       model:                  community metabolic models as struct object
+%       abTable:                table containing the relative abundance of
+%                               each organism
+% Output:
+%       solution:               Solution, returned as a real vector or real 
+%                               array.
+
+% find model number
+num = extractBetween(abTable.Genome,'KG','_');
+
+% define lower and upper bounds:
+ub = model.ub;
+lb = model.lb;
+
+% add constraint for export reactions
+export = model.rxns(contains(model.rxns, 'export_'));
+
+for i = 1:numel(export)
+    
+    t = char(export{i});
+    inx = strfind(t,'_');
+    n = t(inx(2)+1:end);
+
+    ex = find(strcmp(model.rxns,export{i}));
+    ub(ex) = ub(ex) * abTable.relative_ab(find(contains(abTable.Genome,['KG',n,'_genomic'])));
+    
+    clear ex t inx n 
+end
+
+% define the objective:
+f = zeros(size(model.S,2),1);
+
+for i = 1:numel(num)
+    bio = find(ismember(model.rxns,['BIOMASS_Reaction_',num{i}]));
+    f(bio) = -1 * abTable.relative_ab(find(contains(abTable.Genome,['KG',num{i},'_genomic'])));
+    % set up minimum growth rate
+    lb(bio) = 0.001;
+    clear bio
+end
+
+% equality constraints:
+% LHS
+Aeq = model.S;
+% RHS
+beq = zeros(size(model.S,1),1);
+
+% using Gurobi solver
+problem.A = Aeq;
+problem.rhs = beq;
+problem.obj = f;
+problem.lb = lb;
+problem.ub = ub;
+problem.sense = repelem('=',size(beq,1),1);
+problem.vtype = repelem('C',size(Aeq,2),1);
+
+solution = gurobi(problem);
+
+% using linprog
+%[solution, max_growth, xflag] = linprog(f, [], [], Aeq, beq, lb, ub);
+
+end
