af354461b251afdf5c331a7da78dff8aab5cde01
Yun-Li Hsieh <hsieh5709@calculon.mpimp-golm.mpg.de>
2024-09-05 11:27:11 +0200

    Code for model generation

---
+++ Program/matlab/model_generation/gapseq_model_conversion.m
@@ -0,0 +1,57 @@
+% Convert all models to a uniform standard
+%% gapseq
+% Tables
+tablesDir = '~/IMIC/Program/matlab/COMMIT/data/tables';
+topDir = '~/IMIC/study_case';
+
+formulaeFile = fullfile(tablesDir, 'MNXref', 'MNXref_MET_FORMULAE.csv');
+geneIDFile = fullfile(topDir, 'table','rna_ab_and_geneID_1-1_0h_1.csv');
+
+ID_list = readtable(geneIDFile, 'ReadVariableNames',true);
+formulaTab = readtable(formulaeFile, 'ReadVariableNames', true, 'Delimiter', '\t');
+translationDB = loadTranslationDB;
+
+fprintf('################# Converting gapseq draft models #################\n')
+  
+gapseqModelDir = fullfile(topDir, 'models/gapseq');
+DraftWorkspace = fullfile(topDir, 'models/gapseq/gapseq_models_draft.mat');
+ModelWorkspace = fullfile(topDir, 'models/gapseq/gapseq_models.mat');
+gapseqModels = dir(fullfile(gapseqModelDir, '*.xml'));
+gapseqModels = {gapseqModels.name};
+%names = cellfun(@(x)regexp(x, strcat('cds_KG*[0-9]*[^\-]*'), 'match'),gapseqModels);
+names = cellfun(@(x)regexp(x, strcat('cds_*[\w]*'), 'match'),gapseqModels);
+
+n = numel(gapseqModels);
+    
+for j=1:n
+    fprintf('Reading file #%d/%d: %s\n', j, n, gapseqModels{j})
+    eval(strcat(names{j},'=','readCbModel(fullfile(gapseqModelDir,gapseqModels{j}));'))
+end
+
+% Collect the single models into a cell array 'models'
+eval(strcat('models = {', strjoin(names, ';'),'};'))
+
+% make the stoichiometric matridces sparse to be able to save the models
+% properly
+for j=1:n
+    models{j}.S = sparse(models{j}.S);
+    % Change model ID
+    %models{j}.modelID =  strcat('KG_',num2str(sscanf(extractAfter(models{j}.modelID,'_'),'KG%d')));
+    models{j}.modelID = extractAfter(models{j}.modelID, '_');
+end
+    
+save(DraftWorkspace, 'models')
+
+% convert the model to the the standard format
+fprintf('\nConverting to standard format and translating to MNXref namespace\n')
+for j=1:numel(models)
+    fprintf('\nModel %d/%d:\n', num2str(j))
+    models{j} = convertgapseqModel(models{j}, true, translationDB);
+    % add a formula
+    models{j} = addMetFormulae(models{j}, formulaTab);
+    % change gene ID
+    models{j} = gapseq_gene_IDs(models{j},ID_list);
+end
+    
+save(ModelWorkspace, 'models');
+
---
+++ Program/matlab/utils/carveme_gene_IDs.m
@@ -0,0 +1,56 @@
+function model = carveme_gene_IDs(model,ID_list)
+% Convert gene IDs in CarveMe model to a form that 
+% is used in transcriptome data.
+% Input:
+%           struct model:               metabolic model that has been
+%                                       reconstructed with CarveMe
+% Output:
+%           struct changed_model:       updated CarveMe model
+
+gene = model.genes(contains(model.genes, 'cds'));
+gene = erase(gene, 'lcl|');
+
+% separate contig ID and gene/protein IDs
+gene = split(gene, '.cds.');
+
+% extract gene/protein IDs
+for i = 1:length(gene)
+    t = char(gene{i,2});
+    inx = strfind(t,'.');
+
+    gene{i,2} = t(1:inx(end)-1);
+end
+
+
+Geneid = replace(ID_list.Geneid,'_','.');
+Chr = replace(ID_list.Chr,'_','.');
+proteinID = replace(ID_list.proteinID,'_','.');
+
+IDs = {};
+
+for i = 1:length(gene)
+    % check duplicated protein ID
+    % have protein ID and the duplication does not exit
+    if sum(contains(proteinID,gene{i,2})) == 1
+        IDs{i} = ID_list.Geneid(contains(proteinID,gene{i,2}));
+
+    % does not have protein ID, using gene ID instead
+    elseif sum(contains(proteinID,gene{i,2})) == 0
+        IDs{i} =  ID_list.Geneid(contains(Geneid,gene{i,2}));
+
+    % the duplication does exit, check with contig ID
+    % using cross comparison to find the exact gene name
+    else
+        warning('There are duplicated protein IDs.')
+        IDs{i} = ID_list.Geneid(intersect(find((contains(proteinID,gene{i,2}))), ...
+            find(contains(Chr, gene{i,1}))));
+
+    end
+
+    IDs = [IDs{:}]';
+end
+
+for i = 1:length(gene)
+    model.genes(intersect(find(contains(model.genes,gene{i,2})), ...
+        find(contains(model.genes,gene{i,1})))) = IDs(i);
+end
